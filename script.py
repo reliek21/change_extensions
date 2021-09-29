ea6d3b46fa5f582feae237f86cd1ea51757bf657
Keiler Guardo <82057272+reliek21@users.noreply.github.com>
2021-09-28 19:05:22 -0500

    Update script.py

--- script.py
+++ script.py
@@ -8,5 +8,5 @@ for filename in os.listdir(folder):
     infilename = os.path.join(folder,filename)
     if not os.path.isfile(infilename): continue
     oldbase = os.path.splitext(filename)
-    newname = infilename.replace(extension1, extension2)
+    newname = infilename.replace('.' + extension1, '.' + extension2)
     output = os.rename(infilename, newname)
